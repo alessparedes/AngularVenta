e8c62483f0d26179e8f31a8bb91412e97cb52017
Alessandro Paredes <paredesandro@gmail.com>
2026-08-25 18:23:49 -0600

    Add new features of sales

---
+++ src/app/venta/dialog/dialogventa/dialogventa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogventa',
+  templateUrl: './dialogventa.component.html',
+  styleUrls: ['./dialogventa.component.scss']
+})
+export class DialogventaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
